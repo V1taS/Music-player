415bce3f5b8e14d2cc9c8976e53f23905d3372bf
Vitalii Sosin <375693@mail.ru>
2020-11-19 18:42:22 +0300

    Initial Commit

---
+++ Music player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music player
+//
+//  Created by Виталий Сосин on 19.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
